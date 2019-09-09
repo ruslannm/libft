75bb9186e9001f9993ff5e6887587e311e98dee7
Rueben Gero <rgero@ox-g6.21-school.ru>
2019-09-09 19:48:52 +0300

    putnbr

--- libft.h
+++ libft.h
@@ -6,16 +6,18 @@
 /*   By: rgero <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/08 16:57:15 by rgero             #+#    #+#             */
-/*   Updated: 2019/09/09 18:39:00 by rgero            ###   ########.fr       */
+/*   Updated: 2019/09/09 19:45:18 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <string.h>
 
 void	ft_putchar(char c);
 void    ft_putstr(char *str);
+void	ft_putnbr(int n);
 
 void    *ft_memset(void *b, int c, size_t len);
 void    ft_bzero(void *s, size_t n);
@@ -25,5 +27,6 @@ void    *ft_memccpy(void *restrict dst, const void *restrict src, int c, \
 void    *ft_memmove(void *dst, const void *src, size_t len);
 void    *ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t	strlen(const char *s);
 
 #endif
