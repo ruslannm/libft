ac66243e7a39a375bc85ace9d20b2db6e5972b44
Rueben Gero <rgero@ox-f4.21-school.ru>
2019-09-10 19:01:40 +0300

    strncat

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rgero <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/08 16:57:15 by rgero             #+#    #+#             */
-/*   Updated: 2019/09/10 18:33:57 by rgero            ###   ########.fr       */
+/*   Updated: 2019/09/10 18:48:25 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,5 @@ char    *ft_strdup(const char *s1);
 char    *ft_strcpy(char *dst, const char *src);
 char    *ft_strncpy(char * dst, const char * src, size_t len);
 char    *ft_strcat(char *restrict s1, const char *restrict s2);
-
+char    *ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
 #endif
