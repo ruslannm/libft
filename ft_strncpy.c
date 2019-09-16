3086193ed34d31333f6b8f4f3c322023647e05f9
Rueben Gero <rgero@ox-f4.21-school.ru>
2019-09-16 15:21:41 +0300

    check8

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,29 +6,27 @@
 /*   By: rgero <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/10 16:45:55 by rgero             #+#    #+#             */
-/*   Updated: 2019/09/10 18:17:32 by rgero            ###   ########.fr       */
+/*   Updated: 2019/09/16 14:46:50 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	size_t	i;
-	int		null_term;
 
 	i = 0;
-	null_term = 1;
-	while (i < len)
+	while (src[i] != '\0' && len > 0)
 	{
-		if (src[i] != '\0' && null_term)
-			dst[i] = src[i];
-		else
-		{
-			dst[i] = '\0';
-			null_term = 0;
-		}
+		dst[i] = src[i];
 		i++;
+		len--;
+	}
+	while (len > 0)
+	{
+		dst[i++] = '\0';
+		len--;
 	}
 	return (dst);
 }
--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: rgero <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/12 17:13:54 by rgero             #+#    #+#             */
-/*   Updated: 2019/09/15 14:51:10 by rgero            ###   ########.fr       */
+/*   Updated: 2019/09/16 15:19:42 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,27 @@
 
 int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	char	*s1_tmp;
-	char	*s2_tmp;
-	size_t	i;
+	unsigned char	*s1_tmp;
+	unsigned char	*s2_tmp;
+	size_t			i;
 
 	if (!s1 || !s2)
 		return (0);
-	s1_tmp = (char *)s1;
-	s2_tmp = (char *)s2;
+	s1_tmp = (unsigned char *)s1;
+	s2_tmp = (unsigned char *)s2;
 	i = 0;
-	while (i < n && s1_tmp[i] != '\0' && s2_tmp[i] != '\0' \
-			&& s1_tmp[i] == s2_tmp[i])
+	while (i < n )
 	{
-		i++;
+		if (s1_tmp[i] != '\0' && s2_tmp[i] != '\0')
+			if (s1_tmp[i] == s2_tmp[i])
+				i++;
+			else		
+				return (0);
+		else
+			if (s1_tmp[i] == s2_tmp[i])
+				return (1);
+            else
+                return (0);		
 	}
-	if (s1_tmp[i] == s2_tmp[i])
-		return (1);
-	return (0);
+	return (1);
 }
